f5598e2e71c556db4f444d066031ba194a47b7df
Brian C Sparks <briancsparks@gmail.com>
2022-11-02 10:48:49 -0700

    second

---
+++ inc/comb.h
@@ -0,0 +1,199 @@
+//
+// Created by Brian Sparks on 11/1/22.
+//
+
+#ifndef INC_2_BASIC_COMB_H
+#define INC_2_BASIC_COMB_H
+
+namespace one {
+
+  template <typename Tint>
+  bool nextComb0(Tint * ii, Tint n, Tint r) {
+    int * begin = ii;
+    int * end   = begin + r;
+    int * p     = nullptr;
+
+    // Init
+    if (ii[0] == -1 || ii[r-1] <= 0) {
+      p = begin;
+      *p = 0;
+
+    } else {
+
+      int pmax = n;
+      for (p = end - 1; p >= begin; --p, --pmax) {
+        *p += 1;
+        if (*p < pmax) {
+          break;
+        }
+      }
+      if (p < begin) {
+        return false;
+      }
+    }
+
+
+    for (p += 1; p < end; ++p) {
+      *p = (*(p-1)) + 1;
+    }
+
+    return true;
+  }
+
+  template <typename Tint>
+  void init(Tint * ii, Tint r)
+  {
+    int * p = ii;
+    for (int i = 0; i < r; ++i) {
+      *p++ = -1;
+    }
+  }
+
+  template <typename Tint>
+  bool spring(Tint * ii, Tint n, Tint r, Tint * p, Tint pval)
+  {
+    for (; p < ii + r; ++p) {
+      *p = pval++;
+    }
+    return true;
+  }
+
+  template <typename Tint>
+  bool spring0(Tint * ii, Tint n, Tint r, Tint * p, Tint pval)
+  {
+    *p = pval;
+    for (++p; p < ii + r; ++p) {
+      *p = 0;
+    }
+    return true;
+  }
+
+  template <typename Tint>
+  bool nextComb1(Tint * ii, Tint n, Tint r) {
+    int * begin = ii;
+    int * last  = begin + r - 1;
+    int * p     = nullptr;
+
+    // Init
+    if (ii[0] == -1 || ii[r-1] <= 0) {
+      return spring(ii, n, r, ii, 0);
+    }
+
+    for (p = last; p >= begin; --p) {
+      int revcolnum = (last - p);
+      *p += 1;
+      int max = n - revcolnum;      // -----
+      if (*p < n - revcolnum) {                    // n - revcolnum is the max for the column
+        return spring(ii, n, r, p, *p);
+      }
+    }
+
+    return false;
+  }
+
+  template <typename Tint>
+  bool nextComb2(Tint * ii, Tint n, Tint r) {
+    int * last  = ii + r - 1;
+
+    // Init
+    if (ii[0] == -1 || ii[r-1] <= 0) {
+      return spring(ii, n, r, ii, 0);
+    }
+
+    int reverseColNum = 0;
+    for (int * p = last; p >= ii; --p, ++reverseColNum) {
+      *p += 1;
+      int max = n - reverseColNum;      // -----
+      if (*p < n - reverseColNum) {                    // n - reverseColNum is the max for the column
+        return spring(ii, n, r, p, *p);
+      }
+    }
+
+    return false;
+  }
+
+  template <typename Tint>
+  bool nextComb(Tint * ii, Tint n, Tint r) {
+    return nextComb2(ii, n, r);
+  }
+
+  template <typename Tint>
+  class comb {
+  public:
+    Tint * arr;
+    Tint n, r;
+    comb(Tint n, Tint r) : arr(nullptr), n(n), r(r) {}
+
+    ~comb()
+    {
+      delete arr;
+    }
+
+    bool init()
+    {
+      arr = new Tint[r];
+      return spring(arr, n, r, arr, 0);
+    }
+
+    bool next()
+    {
+      if (arr == nullptr) {
+        return init();
+      }
+
+      int * last  = arr + r - 1;
+
+      int max = n;
+      for (int * p = last; p >= arr; --p, --max) {
+        *p += 1;
+        if (*p < max) {
+          return spring(arr, n, r, p, *p);
+        }
+      }
+
+      return false;
+    }
+  };
+
+  template <typename Tint>
+  class perm {
+  public:
+    Tint * arr;
+    Tint n, r;
+    perm(Tint n, Tint r) : arr(nullptr), n(n), r(r) {}
+
+    ~perm()
+    {
+      delete arr;
+    }
+
+    bool init()
+    {
+      arr = new Tint[r];
+      return spring0(arr, n, r, arr, 0);
+    }
+
+    bool next()
+    {
+      if (arr == nullptr) {
+        return init();
+      }
+
+      int * last  = arr + r - 1;
+
+      int max = n;
+      for (int * p = last; p >= arr; --p, --max) {
+        *p += 1;
+        if (*p < max) {
+          return spring0(arr, n, r, p, *p);
+        }
+      }
+
+      return false;
+    }
+  };
+
+}
+
+
+#endif //INC_2_BASIC_COMB_H
--- main.cpp
+++ main.cpp
@@ -1,37 +1,8 @@
 #include <iostream>
 
-bool next(int * ii, int n, int r)
-{
-  int * begin = ii;
-  int * end   = begin + r;
-  int * p     = nullptr;
+#include "inc/comb.h"
 
-  // Init
-  if (ii[0] == -1 || ii[r-1] == 0) {
-    p = ii;
-    for (int i = 0; i < r; ++i) {
-      *p++ = i;
-    }
-    return true;
-  }
-
-  int pmax = n;
-  for (p = end - 1; p >= begin; --p, --pmax) {
-    *p += 1;
-    if (*p < pmax) {
-      break;
-    }
-  }
-  if (p < begin) {
-    return false;
-  }
-
-  for (p += 1; p < end; ++p) {
-    *p = (*(p-1)) + 1;
-  }
-
-  return true;
-}
+using namespace one;
 
 void init(int * ii, int r)
 {
@@ -41,18 +12,104 @@ void init(int * ii, int r)
   }
 }
 
+void assertN3(int * ii, int a, int b, int c) {
+  assert(ii[0] == a);
+  assert(ii[1] == b);
+  assert(ii[2] == c);
+}
+
+// 5c3:
+// 0:  [0, 1, 2]                            [0, 1, 2]
+// 1:  [0, 1, 3]                            [0, 1, 3]
+// 2:  [0, 1, 4]                            [0, 1, 4]
+// 3:  [0, 2, 3]                       <--   !!! [0, 1, 5]
+// 4:  [0, 2, 4]                            [0, 2, 4]
+// 5:  [0, 3, 4]                       <--   !!! [0, 2, 5]
+// 6:  [1, 2, 3]  <--   !!! [0, 4, 5]  <--   !!! [0, 3, 5]
+// 7:  [1, 2, 4]                            [1, 2, 4]
+// 8:  [1, 3, 4]                       <--   !!! [1, 2, 5]
+// 9:  [2, 3, 4]  <--   !!! [1, 4, 5]  <--   !!! [1, 3, 5]
+
+
 int main() {
   int n = 5, r = 3;
-  int arr[r];
-  init(arr, r);
+  auto combins = comb<int>(n, r);
 
-  std::cout << "Hello, World!" << std::endl;
-  while (next(arr, n, r)) {
-    for (int i = 0; i < r; ++i) {
-      std::cout << arr[i]+1 << ", ";
-    }
-    std::cout << std::endl;
+  assert(combins.next());        // 0
+  assertN3(combins.arr, 0,1,2);
+  assert(combins.next());        // 1
+  assert(combins.next());        // 2
+  assert(combins.next());        // 3
+  assertN3(combins.arr, 0,2,3);
+  assert(combins.next());        // 4
+  assert(combins.next());        // 5
+  assert(combins.next());        // 6
+  assertN3(combins.arr, 1,2,3);
+  assert(combins.next());        // 7
+  assert(combins.next());        // 8
+  assert(combins.next());        // 9
+  assertN3(combins.arr, 2,3,4);
+  assert(!combins.next());        // 10
+
+
+
+  auto permus = comb<int>(r, r);
+
+  assert(permus.next());        // 0
+//  assertN3(permus.arr, 0, 0, 0);
+  assert(permus.next());        // 1
+  assert(permus.next());        // 2
+  assert(permus.next());        // 3
+  assert(permus.next());        // 4
+  assert(permus.next());        // 5
+//  assertN3(permus.arr, 0, 1, 0);
+  assert(permus.next());        // 6
+  assert(permus.next());        // 7
+  assert(permus.next());        // 8
+  assert(permus.next());        // 9
+  assert(permus.next());        // 10
+  for (int i = 0; i < 45; ++i) {
+    assert(permus.next());        // 10
   }
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+  assert(permus.next());        // 10
+
+
+//  int arr[r];
+//
+//  init(arr, r);
+//  assert(nextComb(arr, n, r));        // 0
+//  assertN3(arr, 0,1,2);
+//  assert(nextComb(arr, n, r));        // 1
+//  assert(nextComb(arr, n, r));        // 2
+//  assert(nextComb(arr, n, r));        // 3
+//  assertN3(arr, 0,2,3);
+//  assert(nextComb(arr, n, r));        // 4
+//  assert(nextComb(arr, n, r));        // 5
+//  assert(nextComb(arr, n, r));        // 6
+//  assertN3(arr, 1,2,3);
+//  assert(nextComb(arr, n, r));        // 7
+//  assert(nextComb(arr, n, r));        // 8
+//  assert(nextComb(arr, n, r));        // 9
+//  assertN3(arr, 2,3,4);
+//  assert(!nextComb(arr, n, r));        // 10
+
+//  init(arr, r);
+//
+//  std::cout << "Hello, World!" << std::endl;
+//  while (nextComb(arr, n, r)) {
+//    for (int i = 0; i < r; ++i) {
+//      std::cout << arr[i] /*+1*/ << ", ";
+//    }
+//    std::cout << std::endl;
+//  }
 
   return 0;
 }
